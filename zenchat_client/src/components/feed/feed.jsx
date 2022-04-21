8f1ec7ad7bbd8f0ed2ebd6d80df53d6a0a03250e
unknown <tzenisekj@gmail.com>
2022-04-20 17:27:28 -0700

    created post component and stylized

--- zenchat_client/src/components/feed/feed.jsx
+++ zenchat_client/src/components/feed/feed.jsx
@@ -1,10 +1,12 @@
 import "./feed.css"
 import Share from "../share/share"
+import Post from "../post/post"
 
 export default function Feed() {
   return (
     <div className="feed-container">
      <Share />
+     <Post />
     </div>
   )
 }
--- zenchat_client/src/components/post/post.jsx
+++ zenchat_client/src/components/post/post.jsx
@@ -1,7 +1,35 @@
 import "./post.css"
+import { MoreVert, Favorite, ThumbUp } from "@material-ui/icons"
 
-export default function post() {
+export default function Post() {
   return (
-    <div>post</div>
+    <div className="post">
+        <div className="post-container">
+        <div className="post-top">
+            <div className="post-top-left">
+                <img src="" alt="" className="post-profile-img" />
+                <span className="post-username">Fill Fill</span>
+                <span className="post-date">5 min ago</span>
+            </div>
+            <div className="post-top-right">
+                <MoreVert />
+            </div>
+        </div>
+        <div className="post-center">
+            <span className="post-text">Post Text</span>
+            <img src="" alt="" className="post-img" />
+        </div>
+        <div className="post-bottom">
+            <div className="post-bottom-left">
+                <Favorite htmlColor="red" className="post-icon" />
+                <ThumbUp htmlColor="blue" classname="post-icon" />
+                <span className="post-like-counter">0 Likes</span>
+            </div>
+            <div className="post-bottom-right">
+                <span className="post-comment-counter">0 Comments</span>
+            </div>
+        </div>
+        </div>
+    </div>
   )
 }
